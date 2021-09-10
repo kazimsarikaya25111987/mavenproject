6b2aac759628c0597eede37189b4434fc8bc62c3
kzmsrky87 <kzmsrky87@outlook.com>
2021-09-10 20:36:29 +0200

    started hello

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,5 +2,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("developing hello");
+        System.out.println("GitHub Repo is already publidhe on github");
     }
 }
